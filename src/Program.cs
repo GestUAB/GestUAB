0254574ccd8f3babdbc883cf13211235ad259f90
Tony Alexander Hild <tony_hild@yahoo.com>
2012-07-03 15:45:06 -0300

    Self hosting running with nginx

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
+using Nancy.Hosting.Self;
+using System.Linq;
+using System.Threading;
 
 namespace GestUAB
 {
@@ -7,10 +9,25 @@ namespace GestUAB
     {
         static void Main (string[] args)
         {
-            Console.WriteLine ("Listening on port 8888");
-            Console.WriteLine ("Press CTRL+C to quit :-)");
-            Process.Start ("http://127.0.0.1:8888/");
-            Starter.Start (8888, true);
+//            Console.WriteLine ("Listening on port 8888");
+//            Console.WriteLine ("Press CTRL+C to quit :-)");
+//            Process.Start ("http://127.0.0.1:8888/");
+//            Starter.Start (8888, true);
+
+            // initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+            var host = new NancyHost (new Uri ("http://127.0.0.1:8888"));
+            host.Start (); // start hosting
+
+            //Under mono if you deamonize a process a Console.ReadLine with cause an EOF
+            //so we need to block another way
+            if (args.Any (s => s.Equals ("-d", StringComparison.CurrentCultureIgnoreCase))) {
+                while (true)
+                    Thread.Sleep (10000000); 
+            } else {
+                Console.ReadKey ();
+            }
+    
+            host.Stop (); // stop hosting
         }
     }
 }
