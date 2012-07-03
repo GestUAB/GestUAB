12e5c933e6d7d73ca7c9e032092053eda8c84f3d
Tony Alexander Hild <tony_hild@yahoo.com>
2012-07-03 15:09:34 -0300

    Initial validation support

--- src/Modules/UserModule.cs
+++ src/Modules/UserModule.cs
@@ -4,6 +4,7 @@ using Raven.Client.Linq;
 using System.Linq;
 using Nancy.Responses;
 using Nancy.ModelBinding;
+using Nancy.Validation;
 
 namespace GestUAB.Modules
 {
@@ -13,26 +14,20 @@ namespace GestUAB.Modules
         public UserModule ()
         {
             Get ["/users"] = _ => { 
-                //http://stackoverflow.com/questions/5399967/parse-string-into-a-linq-query
-                //http://www.codeproject.com/Articles/43678/Dynamically-evaluated-SQL-LINQ-queries
-                return View ["User/users",
-                DocumentSession.Query<User> ().ToList ()];
+                return View ["User/users", DocumentSession.Query<User> ().ToList ()];
             };
     
             Get ["/user/{Username}"] = x => { 
                 var username = (string)x.Username;
-//                var user = Database.Instance.ReadAll<UserModel> ().Where(u => u.Username == username).FirstOrDefault();
                 var user = DocumentSession.Query<User> ("UsersByUsername")
                     .Where (n => n.Username == username).FirstOrDefault ();
                 if (user == null)
                     return new NotFoundResponse ();
-//                Context.EnableOutputCache(int.MaxValue);
                 return View ["User/user", user];
             };
 
             Get ["/user/update/{Username}"] = x => {
                 var username = (string)x.Username;
-//                var user = Database.Instance.ReadAll<UserModel> ().Where(u => u.Username == username).FirstOrDefault();
                 var user = DocumentSession.Query<User> ("UsersByUsername")
                     .Where (n => n.Username == username).FirstOrDefault ();
                 if (user == null) 
@@ -53,7 +48,6 @@ namespace GestUAB.Modules
                     saved,
                     new DefaultJsonSerializer ()
                 );
-//                Context.DisableOutputCache();
                 resp.Headers.Add ("Location", "/user/" + saved.Username);
                 resp.StatusCode = HttpStatusCode.OK;
                 return resp;
@@ -65,6 +59,10 @@ namespace GestUAB.Modules
 
             Post ["/user/create"] = x => {
                 var user = this.Bind<User> ();
+                var result = this.Validate(user);
+                if (!result.IsValid) {
+                    return View["Shared/_errors", result];
+                }
                 DocumentSession.Store (user);
                 var resp = new JsonResponse<User> (
                     user,
@@ -73,24 +71,15 @@ namespace GestUAB.Modules
                 resp.Headers.Add ("Location", "/user/" + user.Username);
                 resp.StatusCode = HttpStatusCode.Created;
                 return resp;
-//                return Response.AsRedirect("/users");
-//                return View ["User/users", Database.Instance.Read<UserModel> ().ToArray ()];
             };
 
             Delete ["/user/delete/{Username}"] = x => { 
-//                string message = "";
                 var username = (string)x.Username;
                 var user = DocumentSession.Query<User> ("UsersByUsername")
                         .Where (n => n.Username == username)
                         .FirstOrDefault ();
                 if (user == null) 
                     return new NotFoundResponse ();
-//                if (user == default(UserModel)) {
-//                    message = "Usuário " + x.Username + " excluído!";
-//                    Database.Instance.Delete (user);
-//                } else {
-//                    message = "Usuário " + x.Username + " não encontrado!";
-//                }
                 DocumentSession.Delete (user);
                 var resp = new JsonResponse<User> (
                         user,
