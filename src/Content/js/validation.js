282433cc45fd08fd3b46b645f34bce9519283dd5
Tony Alexander Hild <tony_hild@yahoo.com>
2012-09-03 09:27:11 -0300

    -Update jquery version to 1.8.1. -Standarized module actions and view names. -Added Id of GUID type to IModel interface. -Other minor fixes.

--- src/Content/js/validation.js
+++ src/Content/js/validation.js
@@ -25,6 +25,28 @@
 // THE SOFTWARE.
 
 var page = function () {
+/*
+    jQuery.extend(jQuery.validator.messages, {
+        required: "This field is required.",
+        remote: "Please fix this field.",
+        email: "Please enter a valid email address.",
+        url: "Please enter a valid URL.",
+        date: "Please enter a valid date.",
+        dateISO: "Please enter a valid date (ISO).",
+        number: "Please enter a valid number.",
+        digits: "Please enter only digits.",
+        creditcard: "Please enter a valid credit card number.",
+        equalTo: "Please enter the same value again.",
+        accept: "Please enter a value with a valid extension.",
+        maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+        minlength: jQuery.validator.format("Please enter at least {0} characters."),
+        rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+        range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+        max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+        min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+    });
+*/
+
     $.validator.addMethod('alpha', function (value, element, params) {
         return (/^[a-zA-Z]+$/.test(value));
     });
@@ -33,6 +55,26 @@ var page = function () {
         options.rules['alpha'] = true;
         options.messages['alpha'] = options.message;
     });
+
+    $.validator.unobtrusive.adapters.add("lettersonly", {}, function(options) {
+        options.rules['lettersonly'] = true;
+        options.messages['lettersonly'] = options.message;
+    });
+            
+    /*
+    $.validator.addMethod('matches', function (value, element, params) {
+        return ((new RegExp(params.regex)).test(value));
+    });
+    
+    $.validator.unobtrusive.adapters.add("matches", ["regex"], function(options) {
+        var params = {
+            regex: options.params.regex.split(',')
+        };
+        options.rules['matches'] = params;
+        options.messages['matches'] = options.message;
+    });
+    */
+        
     //Update that validator
     $.validator.setDefaults({
         highlight: function (element) {
@@ -43,3 +85,72 @@ var page = function () {
         }
     });
 } ();
+
+(function($){
+    $.fn.toJSON = function(options){
+
+        options = $.extend({}, options);
+
+        var self = this,
+            json = {},
+            push_counters = {},
+            patterns = {
+                "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+                "key":      /[a-zA-Z0-9_]+|(?=\[\])/g,
+                "push":     /^$/,
+                "fixed":    /^\d+$/,
+                "named":    /^[a-zA-Z0-9_]+$/
+            };
+
+
+        this.build = function(base, key, value){
+            base[key] = value;
+            return base;
+        };
+
+        this.push_counter = function(key){
+            if(push_counters[key] === undefined){
+                push_counters[key] = 0;
+            }
+            return push_counters[key]++;
+        };
+
+        $.each($(this).serializeArray(), function(){
+
+            // skip invalid keys
+            if(!patterns.validate.test(this.name)){
+                return;
+            }
+
+            var k,
+                keys = this.name.match(patterns.key),
+                merge = this.value,
+                reverse_key = this.name;
+
+            while((k = keys.pop()) !== undefined){
+
+                // adjust reverse_key
+                reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+                // push
+                if(k.match(patterns.push)){
+                    merge = self.build([], self.push_counter(reverse_key), merge);
+                }
+
+                // fixed
+                else if(k.match(patterns.fixed)){
+                    merge = self.build([], k, merge);
+                }
+
+                // named
+                else if(k.match(patterns.named)){
+                    merge = self.build({}, k, merge);
+                }
+            }
+
+            json = $.extend(true, json, merge);
+        });
+
+        return json;
+    };
+})(jQuery);
