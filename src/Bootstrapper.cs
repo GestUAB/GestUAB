84aa906fec28e22264663572d5c7fbdd5a2bc7f2
viniciusbord9 <viniciusbord9@gmail.com>
2012-11-18 14:04:42 -0200

    add file log

--- src/Bootstrapper.cs
+++ src/Bootstrapper.cs
@@ -42,10 +42,11 @@ namespace GestUAB
 
         protected override void ApplicationStartup (TinyIoC.TinyIoCContainer container, IPipelines pipelines)
         {
-#if (DEBUG)
             base.ApplicationStartup (container, pipelines);
+#if (DEBUG)
             StaticConfiguration.DisableErrorTraces = false;
             StaticConfiguration.DisableCaches = true;
+            StaticConfiguration.EnableRequestTracing = true;
 #endif
         }
 
