1ceeb44417efaa9994dbe1c6c899b76c90a3c722
Felipe Donatti <felipedonatti@hotmail.com>
2012-11-30 21:47:52 -0200

    Desistence module

---
+++ src/Models/Desistence.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using FluentValidation;
+using Raven.Client;
+using Raven.Client.Linq;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using GestUAB.Validators;
+using GestUAB.Models;
+
+namespace GestUAB.Models
+{
+    /// <summary>
+    /// Desistence Class
+    /// </summary>
+    /// 
+    public class Desistence : IModel
+    {
+        #region Variables
+        [Display(Name = "Código",
+            Description = "Código da desistencia.")]
+        [ScaffoldVisibility(all:ScaffoldVisibilityType.Hidden)]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Professor",
+            Description = "Professor.")]
+        [ScaffoldVisibility(all: ScaffoldVisibilityType.Show)]
+        public string NameTeacher { get; set; }
+
+        [Display(Name = "Data",
+            Description = "Data da desistencia")]
+        [ScaffoldVisibility(all: ScaffoldVisibilityType.Show)]
+        public string TravelDate { get; set; }
+
+        [Display(Name = "Destino",
+            Description = "Destino da viagem")]
+        [ScaffoldVisibility(all: ScaffoldVisibilityType.Show)]
+        public string Destiny { get; set; }
+
+        [Display(Name = "Motivo",
+            Description = "Motivo da desistencia")]
+        [ScaffoldVisibility(all: ScaffoldVisibilityType.Show)]
+        public string ReasonGiveup { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Class Builder
+        /// </summary>
+        public Desistence()
+        {
+
+        }
+
+        /// <summary>
+        /// Static method that creates a default Desistence
+        /// </summary>
+        /// <returns></returns>
+        public static Desistence DefaultDesistence()
+        {
+            return new Desistence() {
+                Id = Guid.NewGuid(),
+                NameTeacher = string.Empty,
+                TravelDate = DateTime.Now.ToString(),
+                Destiny = string.Empty,
+                ReasonGiveup = string.Empty
+            };
+        }
+    }
+
+    /// <summary>
+    /// Desistence Validator Class
+    /// </summary>
+    public class DesistenceValidator : ValidatorBase<Desistence>
+    {
+        public DesistenceValidator()
+        {
+            #region New Desistence
+            using (var session = DocumentSession)
+            {
+                RuleFor(desistence => desistence.Id).NotEmpty();
+
+                RuleFor(desistence => desistence.NameTeacher)
+                    .NotEmpty().WithMessage("Nome do professor é obrigatório.");
+                    //.Must((desistence, teacherName) => !session.Query<Teacher>()
+                    //    .Where(n => n.Name != teacherName).Any())
+                    //    .WithMessage("Nome do professor não cadastrado.");
+                
+                RuleFor(desistence => desistence.TravelDate)
+                    .NotEmpty().WithMessage("Preencha da data.")
+                    .Length(10)
+                    .Matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}$")
+                    .WithMessage("O formato deve seguir o seguinte padrão: 01/01/0001 ");
+
+                RuleFor(desistence => desistence.Destiny).NotEmpty()
+                    .WithMessage("Informe o destino.");
+
+                RuleFor(desistence => desistence.ReasonGiveup).NotEmpty()
+                    .WithMessage("Informe o destino.");
+
+            }
+            #endregion
+
+            #region Update
+            RuleSet("Update", () => {
+                RuleFor(desistence => desistence.NameTeacher)
+                       .NotEmpty().WithMessage("Nome do professor é obrigatório.");
+
+                RuleFor(desistence => desistence.TravelDate).NotEmpty()
+                    .Length(10)
+                    .Matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}$")
+                    .WithMessage("O formato deve seguir o seguinte padrão: 01/01/0001 ");
+
+                RuleFor(desistence => desistence.Destiny).NotEmpty()
+                   .WithMessage("Informe o destino.");
+
+                RuleFor(desistence => desistence.ReasonGiveup).NotEmpty()
+                    .WithMessage("Informe o destino.");
+
+            });
+            
+            #endregion
+        }
+    }
+
+}
---
+++ src/Modules/DesistenceModule.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Nancy;
+using GestUAB.Models;
+using Raven.Client.Linq;
+using System.Linq;
+using Nancy.ModelBinding;
+using Nancy.Validation;
+using FluentValidation;
+using Nancy.Responses;
+
+namespace GestUAB.Modules
+{
+    public class DesistenceModule : BaseModule
+    {
+        public DesistenceModule() : base("/desistences")
+        {
+            #region Method that returns the index View Desistence, with the registered desistence.
+            Get["/"] = _ => {
+                return View["index", DocumentSession.Query<Desistence>()
+                    .Customize(q => q.WaitForNonStaleResultsAsOfLastWrite())
+                    .ToList()];
+            };
+            #endregion
+
+            #region  Method that returns a View Show, displaying the desistence in the form according to the ID.
+            Get["/{Id}"] = x => {
+                Guid desistenceId = Guid.Parse(x.Id);
+                var desistence = DocumentSession.Query<Desistence>("DesistenceById")
+                    .Customize(q => q.WaitForNonStaleResultsAsOfLastWrite())
+                    .Where(n => n.Id == desistenceId).FirstOrDefault();
+                if (desistence == null)
+                    return new NotFoundResponse();
+                return View["show", desistence];
+            };
+            #endregion
+
+            #region Method that returns the New View, creating a default desistence
+            Get["/new"] = x => {
+                return View["new", Desistence.DefaultDesistence()];
+            };
+            #endregion
+
+            #region Method that creates and validates a new desistence in accordance with the specifications of the class DesistenceValidator
+            Post["/new"] = x => {
+                var desistence = this.Bind<Desistence>();
+                var result = new DesistenceValidator().Validate(desistence);
+                if (!result.IsValid)
+                    return View["Shared/_errors", result];
+                DocumentSession.Store(desistence);
+                return Response.AsRedirect(string.Format("/desistences/{0}", desistence.Id));
+            };
+            #endregion
+
+            #region Displays data in the form of the Desistence according to ID
+            Get["/edit/{Id}"] = x => {
+                Guid desistenceId = Guid.Parse(x.Id);
+                var desistence = DocumentSession.Query<Desistence>("DesistenceById")
+                    .Where(n => n.Id == desistenceId).FirstOrDefault();
+                if (desistence == null)
+                    return new NotFoundResponse();
+                return View["edit", desistence];
+            };
+            #endregion
+
+            #region Method editing the desistence according to ID
+            Post["/edit/{Id}"] = x => {
+                var desistence = this.Bind<Desistence>();
+                var result = new DesistenceValidator().Validate(desistence, ruleSet: "Update");
+                if (!result.IsValid)
+                    return View["Shared/_errors", result];
+                Guid desistenceId = Guid.Parse(x.Id);
+                var saved = DocumentSession.Query<Desistence>("DesistenceById")
+                    .Where(n => n.Id == desistenceId).FirstOrDefault();
+                if (saved == null)
+                    return new NotFoundResponse();
+                saved.Fill(desistence);
+                return Response.AsRedirect(string.Format("/desistences/{0}", desistence.Id));
+            };
+            #endregion
+
+            #region Method to delete a record according to ID
+            Get["/delete/{Id}"] = x => 
+            {
+                Guid desistenceId = Guid.Parse(x.Id);
+                var desistence = DocumentSession.Query<Desistence>("DesistenceById")
+                    .Where(n => n.Id == desistenceId).FirstOrDefault();
+                if (desistence == null)
+                    return new NotFoundResponse();
+                DocumentSession.Delete(desistence);
+                return Response.AsRedirect("/desistences");
+            };
+            #endregion
+        }
+    }
+}
