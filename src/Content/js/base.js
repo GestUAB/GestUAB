a6d24a9883e55637738c3cbe111a126746072eeb
Alessandro Dias Batista <alebatista90@terra.com.br>
2012-12-02 00:04:27 -0200

    separar js do html

--- src/Content/js/base.js
+++ src/Content/js/base.js
@@ -1,3 +1,10 @@
 $(function(){
     $('.carousel').carousel();
+    $("#back_button, #cancel_button").click(function () {
+    	history.go(-1);
+    });
+
+    $("#remove_button").click(function () {
+    	return confirm("Deseja Remover?");
+    });
 });
