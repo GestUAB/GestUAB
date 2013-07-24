324598fbdf4760662f91dc5496e0a20a2c571f92
Tony Alexander Hild <tony_hild@yahoo.com>
2013-07-24 13:06:35 -0200

    Removido TipoCurso

--- src/GestUAB.Migrations/gestuab.sql
+++ src/GestUAB.Migrations/gestuab.sql
@@ -80,18 +80,6 @@ CREATE TABLE IF NOT EXISTS [Curso]
     [TipoCurso] INTEGER NOT NULL CHECK ([Descricao] IN (1, 2, 3, 4, 5, 6, 7, 8, 9)) --"1 Aperfeiçoamento", "2 Bacharelado", "3 Lato Sensu", "4 Licenciatura", "5 Extensão", "6 Sequencial", "7 Tecnólogo", "8 Mestrado", "9 Doutorado" 
 );
 
---SELECT lower(hex(randomblob(16)))
---http://www.guidgenerator.com/online-guid-generator.aspx
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("9a2db218-2b27-4284-b61d-084a7a99147b", "Aperfeiçoamento");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("a2ae5467-3a88-4c62-a930-92a841c3a82e", "Bacharelado");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("c0f649de-d8bc-443a-99d6-08f8e107c94f", "Lato Sensu");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("bdfec130-9f23-4297-8e72-095b2b975cae", "Licenciatura");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("c8382392-ae30-4aa6-8b39-c1e66e739b0e", "Extensão");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("40ba5360-b1a5-46f9-941e-37fd83cd025b", "Sequencial");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("b1cfabc2-159f-4aa6-a541-81180e0fba1f", "Tecnólogo");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("01dde289-79a1-4a3f-b569-9cd81cf71a01", "Mestrado");
-INSERT INTO TipoCurso (Id, Descricao) VALUES ("7f057cc4-b4d6-460e-ac1c-a547c38ba073", "Doutorado");
-
 CREATE TABLE IF NOT EXISTS [VinculoCurso]
 (
     [ColaboradorId] GUID REFERENCES Colaborador(Id),
